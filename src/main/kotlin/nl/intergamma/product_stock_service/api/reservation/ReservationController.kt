45cc87f38d8dc63898ab9664b7734492c899e722
maxbroens <max.broens@rabobank.nl>
2024-10-01 22:02:40 +0200

    initial commit

--- src/main/kotlin/nl/intergamma/product_stock_service/api/reservation/ReservationController.kt
+++ src/main/kotlin/nl/intergamma/product_stock_service/api/reservation/ReservationController.kt
@@ -1,7 +1,6 @@
 package nl.intergamma.product_stock_service.api.reservation
 
 import nl.intergamma.product_stock_service.service.ReservationService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -18,15 +17,13 @@ class ReservationController(
 ) {
 
     @PostMapping
-    fun createReservation(@RequestBody reservationRequest: ReservationRequest): ResponseEntity<ReservationResponse> {
-        val reservation = reservationService.createReservation(reservationRequest)
-        return ResponseEntity.ok(reservation)
+    fun createReservation(@RequestBody reservationRequest: ReservationRequest): ReservationResponse {
+        return reservationService.createReservation(reservationRequest)
     }
 
     @GetMapping("/{reservationId}")
-    fun getReservation(@PathVariable reservationId: UUID): ResponseEntity<ReservationResponse> {
-        val reservation = reservationService.getReservation(reservationId)
-        return ResponseEntity.ok(reservation)
+    fun getReservation(@PathVariable reservationId: UUID): ReservationResponse {
+        return reservationService.getReservation(reservationId)
     }
 
     @DeleteMapping("/{reservationId}")
